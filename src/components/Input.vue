5262f317a123ab9042358c3bf8cb641425cc6457
Janavi Anand <janavianand@yahoo.com>
2019-06-19 14:56:48 -0400

    added state

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,33 +1,46 @@
 <template>
     <div class="input">
-        <label for = "name">Name : </label>
-        <input id = "name" v-model = "name"/>
-        <br>
-        <label for="place">Place : </label>
-        <input id="place" v-model="place"/>
-        <br>
-        <label for="animal">Animal : </label>
-        <input id="animal" v-model="animal"/>
-        <br>
-        <label for="thing">Thing : </label>
-        <input id="thing" v-model="thing"/>
+        <form>
+            <label for="name">Name : </label>
+            <input id="name" v-model="name" @input="nameChange" />
+            <br />
+            <label for="place">Place : </label>
+            <input id="place" v-model="place" @input="placeChange" />
+            <br />
+            <label for="animal">Animal : </label>
+            <input id="animal" v-model="animal" @input="animalChange" />
+            <br />
+            <label for="thing">Thing : </label>
+            <input id="thing" v-model="thing" @input="thingChange" />
+        </form>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Input',
-    props:{
-      name:String
+    methods: {
+        nameChange: function(event) {
+            this.$store.commit('changeName', event.target.value);
+        },
+        placeChange: function(event) {
+            this.$store.commit('changePlace', event.target.value);
+        },
+        animalChange: function(event) {
+            this.$store.commit('changeAnimal', event.target.value);
+        },
+        thingChange: function(event) {
+            this.$store.commit('changeThing', event.target.value);
+        }
     }
 };
 </script>
 
 <style>
 input {
-  height:50px;
-  width:250px;
-  border: solid;
-  margin:10px;
+    height: 50px;
+    width: 250px;
+    border: solid;
+    margin: 10px;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,25 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
+    state: {
+        name: [],
+        place: [],
+        animal: [],
+        thing: []
+    },
+    mutations: {
+        changeName(state, name) {
+            state.name.push(name);
+        },
+        changePlace(state, place) {
+            state.place.push(place);
+        },
+        changeAnimal(state, animal) {
+            state.animal.push(animal);
+        },
+        changeThing(state, thing) {
+            state.thing.push(thing);
+        }
+    },
     actions: {}
 });
